8961a530514d6fa655341c1dcfeb8a41b9d1b2e0
Yoshi <yoshimejia00@gmail.com>
2021-11-05 14:30:32 -0400

    commit tree c16f47651275187a717fa6f6a293e995c42ae5c8

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends:[]}, action){
+    switch (action.type) {
+        case "ADD_FRIEND":
+            //for the spread operator, first we add in the outside values of 'state', then we hit the 'friends' array, inside of that array we add in the values of that current array and insert 'action.friends' to populate the new values in addition to the one we added in with ...state.friends
+          return {...state, friends: [...state.friends, action.friend]};
+        case "REMOVE_FRIEND":
+          return { ...state, friends: [...state.friends.filter(friend => friend.id !== action.id)] };
+        default:
+          return state;
+      }
+    
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,11 @@
-export function managePresents(state, action){
+export function managePresents(state={numberOfPresents: 0}, action){
+    switch (action.type) {
+        case 'INCREASE':
+          return { ...state, numberOfPresents: state.numberOfPresents + 1 };
+        // case "DECREASE_COUNT":
+        //   return { count: state.count - 1 };
+        default:
+          return state;
+      }
+    
 }
