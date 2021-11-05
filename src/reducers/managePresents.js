7c97edd00d22a9542ca130c7f6be07e82d528984
Yoshi <yoshimejia00@gmail.com>
2021-11-05 14:31:15 -0400

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -2,8 +2,6 @@ export function managePresents(state={numberOfPresents: 0}, action){
     switch (action.type) {
         case 'INCREASE':
           return { ...state, numberOfPresents: state.numberOfPresents + 1 };
-        // case "DECREASE_COUNT":
-        //   return { count: state.count - 1 };
         default:
           return state;
       }
